a4483a21e36186b2a64dbd7f8e454e9578d97e7c
adamni21 <85580454+adamni21@users.noreply.github.com>
2023-05-22 21:00:43 +0100

    db.migrate

--- sqlite/db.go
+++ sqlite/db.go
@@ -3,6 +3,10 @@ package sqlite
 import (
 	"database/sql"
 	"fmt"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -33,8 +37,75 @@ func (db *DB) Open() error {
 	if _, err := db.db.Exec("PRAGMA foreign_keys = ON;"); err != nil {
 		return fmt.Errorf("enable foreign keys: %w", err)
 	}
+	if err := db.migrate(); err != nil {
+		return err
+	}
 
 	return nil
+}
+
+type migration = struct {
+	name      string
+	timestamp int32
+}
+
+func (db *DB) migrate() error {
+	currentUserVersion, err := db.currentUserVersion()
+	if err != nil {
+		return fmt.Errorf("getting current user_version: %w", err)
+	}
+
+	fileNames, err := filepath.Glob("./migrations/*.sql")
+	if err != nil {
+		return err
+	}
+
+	files := make([]migration, len(fileNames))
+	for _, name := range fileNames {
+		rawTimestamp := strings.SplitN(name, "_", 2)[0]
+		if len(rawTimestamp) != 10 {
+			return fmt.Errorf("timestamp '%s' has invalid length. filename must begin with padded timestamp", rawTimestamp)
+		}
+
+		// sqlite user_version is a 32 bit int
+		timestamp, err := strconv.ParseInt(rawTimestamp, 10, 32)
+		if err != nil {
+			return fmt.Errorf("parsing timestamp '%s' gave error: %w", rawTimestamp, err)
+		}
+
+		if int32(timestamp) > currentUserVersion {
+			files = append(files, migration{name: name, timestamp: int32(timestamp)})
+		}
+	}
+
+	sort.Slice(files, func(i, j int) bool { return files[i].timestamp < files[j].timestamp })
+	for _, migration := range files {
+		if err := db.migrateFile(migration); err != nil {
+			return fmt.Errorf("migrating file '%s': %w", migration.name, err)
+		}
+	}
+
+	return nil
+}
+
+func (db *DB) migrateFile(migration migration) error {
+	return fmt.Errorf("not implemented yet")
+}
+
+func (db *DB) currentUserVersion() (int32, error) {
+	query, err := db.db.Query("PRAGMA user_version;")
+	if err != nil {
+		return 0, err
+	}
+	defer query.Close()
+
+	var userVersion int32
+	query.Next()
+	if err := query.Scan(&userVersion); err != nil {
+		return 0, err
+	}
+
+	return userVersion, nil
 }
 
 func (db *DB) Close() error {
